3c86c86bd1549be1e0b3a91f4ba612b45ca013b4
ravi bhushan kumar <rbhushan95@gmail.com>
2018-08-23 22:22:39 +0530

    GreatestCommonFactorLogic implemented

---
+++ src/app/common/AlgoPractice/greatestCommonFactor.ts
@@ -0,0 +1,23 @@
+export class GreatestCommonFactor {
+  byDivisionMethod(numberOne: number, numberTwo: number): number {
+    const maxNumber: number = Math.max(numberOne, numberTwo);
+    let minNumber = 0;
+    minNumber =   Math.min(numberOne, numberTwo);
+    const reminder = maxNumber % minNumber;
+    if (reminder === 0) {
+      return minNumber;
+    } else {
+      this.byDivisionMethod(reminder, minNumber);
+    }
+  }
+  bySubstractMethod(numberOne: number, numberTwo: number): number {
+    const maxNumber = Math.max(numberOne, numberTwo);
+    let minNumber = 0;
+    minNumber = Math.min(numberOne, numberTwo);
+    if (maxNumber - minNumber === 0) {
+      return minNumber;
+    } else {
+      this.bySubstractMethod(minNumber, maxNumber - minNumber);
+    }
+  }
+}
--- src/app/employee/empdashboard/empdashboard.component.ts
+++ src/app/employee/empdashboard/empdashboard.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../employee.service';
 import { AuthHttp } from '../../common/servicies/authHttp';
+import { GreatestCommonFactor } from '../../common/AlgoPractice/greatestCommonFactor';
 
 @Component({
   selector: 'app-empdashboard',
   templateUrl: './empdashboard.component.html',
-  styleUrls: ['./empdashboard.component.css'],
-  })
+  styleUrls: ['./empdashboard.component.css']
+})
 export class EmpdashboardComponent implements OnInit {
   constructor(private employeeService: EmployeeService) {}
-
+  greatestCommonFactor: GreatestCommonFactor = new GreatestCommonFactor();
   ngOnInit() {}
   callMe = () => {
-      //  console.log(this.employeeService.getEmployeeList());
-       console.log(this.employeeService.getValueByBasicCall());
+    //  console.log(this.employeeService.getEmployeeList());
+    // console.log(this.employeeService.getValueByBasicCall());
+    console.log(this.greatestCommonFactor.byDivisionMethod(14 , 48));
+    console.log(this.greatestCommonFactor.bySubstractMethod(14 , 48));
   }
 }
